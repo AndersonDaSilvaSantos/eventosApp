7f7922e628fd6e52fed03a69de1295ccb1b9e44a
Anderson Santos <49324173+AndersonDaSilvaSantos@users.noreply.github.com>
2021-04-10 09:33:29 -0300

    Autenticacao via banco de dados funcionando

--- src/main/java/com/eventosApp/EventosAppApplication.java
+++ src/main/java/com/eventosApp/EventosAppApplication.java
@@ -2,12 +2,14 @@ package com.eventosApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class EventosAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventosAppApplication.class, args);
+		System.out.print(new BCryptPasswordEncoder().encode("123"));
 	}
 
 }
--- src/main/java/com/eventosApp/security/WebSecurityConfig.java
+++ src/main/java/com/eventosApp/security/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.eventosApp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -17,21 +20,24 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private ImplementsUserDetailsService userDetailsService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable().authorizeRequests()
-    	.antMatchers(HttpMethod.GET, "/").permitAll()
+		.antMatchers(HttpMethod.GET, "/").permitAll()
+		.antMatchers(HttpMethod.GET, "/cadastrarEvento").hasRole("ADMIN")
+		.antMatchers(HttpMethod.POST, "/cadastrarEvento").hasRole("ADMIN")
 		.anyRequest().authenticated()
 		.and().formLogin().permitAll()
 		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 	
 	@Override
-	@Bean
-	protected UserDetailsService userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder().username("teste").password("000").roles("ADMIN").build();
-		
-		return new InMemoryUserDetailsManager(user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService)
+		.passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 	@Override
